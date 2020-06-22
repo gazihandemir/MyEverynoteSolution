9247eacce2fef3fa594799731e4cbedd56988a81
gazihandemir <gazihand@gmail.com>
2020-06-22 14:02:31 +0300

    22.06.20202

---
+++ MyEvernote.Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEvernote.Entities
+{
+    public class Category
+    {
+        public string  Title{ get; set; }
+        public string Description{ get; set; }
+        public virtual List<Note> Notes{ get; set; }
+    }
+}
