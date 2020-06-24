1c89192f85c613dfdf71e57c62907cce61d2c34e
gazihandemir <gazihand@gmail.com>
2020-06-24 12:46:36 +0300

    24.06.2020

---
+++ MyEvernote.BusinessLayer/Repository.cs
@@ -0,0 +1,35 @@
+﻿using MyEvernote.DataAccessLayer;
+using MyEvernote.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEvernote.BusinessLayer
+{
+    public class Repository<T> where T : class // T tipi class olmak zorunda
+    {
+        private DatabaseContext db = new DatabaseContext();
+        private DbSet<T> _objectSet;
+        public Repository()
+        {
+            _objectSet = db.Set<T>();
+        }
+        public List<T> List()
+        {
+            return _objectSet.ToList();
+        }
+        public int Insert(T obj)
+        {
+            _objectSet.Add(obj);
+            return Save();
+        }
+        public int Save()
+        {
+            return db.SaveChanges();
+        }
+
+    }
+}
