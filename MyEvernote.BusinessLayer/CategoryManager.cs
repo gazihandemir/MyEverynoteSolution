2209f24fe3b9ef74bb5df49017ca5d4ff2387677
gazihandemir <gazihand@gmail.com>
2020-06-26 16:05:59 +0300

    26.06.2020

---
+++ MyEvernote.BusinessLayer/CategoryManager.cs
@@ -0,0 +1,19 @@
+﻿using MyEvernote.DataAccessLayer.EntityFramework;
+using MyEvernote.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEvernote.BusinessLayer
+{
+    public class CategoryManager
+    {
+        private Repository<Category> repo_category = new Repository<Category>();
+        public List<Category> getCategories()
+        {
+            return repo_category.List();
+        }
+    }
+}
