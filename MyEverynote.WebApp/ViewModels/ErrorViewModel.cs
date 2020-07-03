04b3b20313457971a360be7bfe5a85eb787e30a9
gazihandemir <gazihand@gmail.com>
2020-07-03 18:37:27 +0300

    03.07.2020

---
+++ MyEverynote.WebApp/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,13 @@
+﻿using MyEvernote.Entities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyEverynote.WebApp.ViewModels
+{
+    public class ErrorViewModel : NotifyViewModelBase<ErrorMessageObj>
+    {
+
+    }
+}
---
+++ MyEverynote.WebApp/ViewModels/NotifyViewModelBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyEverynote.WebApp.ViewModels
+{
+    public class NotifyViewModelBase<T>
+    {
+        public List<T> Items { get; set; }
+        public string Header { get; set; }
+        public string Title { get; set; }
+        public bool IsRedirecting { get; set; }
+        public string RedirectingUrl { get; set; }
+        public int RedirectingTimeout{ get; set; }
+        public NotifyViewModelBase()
+        {
+            Header = "Yönlendiriliyorsunuz...";
+            Title = "Geçersiz işlem !";
+            IsRedirecting = true;
+            RedirectingUrl = "/Home/Index";
+            TimeoutException = 1000;
+
+        }
+    }
+}
