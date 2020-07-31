a411f7a6e98aeb67488d3fb777f11c4a0ee4964b
gazihandemir <gazihand@gmail.com>
2020-07-31 14:36:35 +0300

    31.07.2020

--- MyEverynote.WebApp/Controllers/CommentController.cs
+++ MyEverynote.WebApp/Controllers/CommentController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using MyEvernote.BusinessLayer;
+using MyEvernote.Entities;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,9 +13,21 @@ namespace MyEverynote.WebApp.Controllers
     public class CommentController : Controller
     {
         // GET: Comment
-        public ActionResult ShowNoteComments()
+        public ActionResult ShowNoteComments(int? id)
         {
-            return PartialView("_PartialComments");
+            NoteManager noteManager = new NoteManager();
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            }
+            //Note note = noteManager.Find(x => x.Id == id);
+            Note note = noteManager.ListQueryable().Include("Comments").FirstOrDefault(x => x.Id == id);
+            if (note == null)
+            {
+                return HttpNotFound();
+            }
+            return PartialView("_PartialComments",note.Comments);
         }
     }
 }
