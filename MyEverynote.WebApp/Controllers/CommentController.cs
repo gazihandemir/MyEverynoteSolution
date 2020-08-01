f29b22d920e255de9fa8fb77910718edef55dad1
gazihandemir <gazihand@gmail.com>
2020-08-01 17:30:57 +0300

    1.08.2020

--- MyEverynote.WebApp/Controllers/CommentController.cs
+++ MyEverynote.WebApp/Controllers/CommentController.cs
@@ -12,11 +12,14 @@ namespace MyEverynote.WebApp.Controllers
 {
     public class CommentController : Controller
     {
+        private NoteManager noteManager = new NoteManager();
+        private CommentManager commentManager = new CommentManager();
+
         // GET: Comment
         public ActionResult ShowNoteComments(int? id)
         {
-            NoteManager noteManager = new NoteManager();
-            if(id == null)
+
+            if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
 
@@ -27,7 +30,31 @@ namespace MyEverynote.WebApp.Controllers
             {
                 return HttpNotFound();
             }
-            return PartialView("_PartialComments",note.Comments);
+            return PartialView("_PartialComments", note.Comments);
+        }
+        [HttpPost]
+        public ActionResult Edit(int? id, string text)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Comment comment = commentManager.Find(x => x.Id == id);
+            if (comment == null)
+            {
+                return new HttpNotFoundResult();
+            }
+            else
+            {
+                comment.Text = text;
+                if (commentManager.Update(comment) > 0)
+                {
+                    return Json(new { result = true }, JsonRequestBehavior.AllowGet);
+                }
+                    return Json(new { result = false }, JsonRequestBehavior.AllowGet);
+                
+            }
         }
     }
 }
